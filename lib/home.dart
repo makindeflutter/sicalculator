ec1417c5b2a21707d9f04aaec3073caf76cbfb6b
makindeflutter <proffakiniyinka@gmail.com>
2022-12-31 16:37:48 +0100

    add

---
+++ lib/home.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/material.dart';
+
+final _currency = ['Naira', 'Dollar', 'Euros'];
+var _currentItemSelected = 'Naira';
+TextEditingController principalController = TextEditingController();
+TextEditingController roiController = TextEditingController();
+TextEditingController termController = TextEditingController();
+var displayResult = '';
